f7a892659cde48e0dfa28c7da85d0a2253a42116
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2024-01-15 23:16:08 +0300

    seeding and all

--- app/app.py
+++ app/app.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
+import os
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
 from models import db, Hero
 
+abs_path = os.getcwd()
+abs_python_path = os.path.normpath(abs_path)
+db_path = f'sqlite:///{abs_path}/db/app.db'
+
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
@@ -15,7 +21,10 @@ db.init_app(app)
 
 @app.route('/')
 def home():
-    return 'from home in app/app.py'
+    hero = Hero(name = 'Mojo Jojo', super_name = 'Crazy Ape')
+    db.session.add(hero)
+    db.session.commit()
+    return 'Hero Added'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
